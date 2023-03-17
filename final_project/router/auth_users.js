2006466100e017dcee2acc67ceda14766bab0829
Mauricio <acencio.mauricio.icinf@gmail.com>
2023-03-17 19:41:54 -0400

    Final exam part 2 task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,29 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    return users.some(user => user.username === username)
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    const user = users.find(user => user.username === username);
+    if (user) {
+        return user.password === password;
+    }
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: "Missing credentials" });
+    }
+    if (!isValid(username) || !authenticatedUser(username, password)) {
+        return res.status(403).json({ message: "Wrong password or username" });
+    }
+    const token = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 })
+    req.session.authorization = { token, username };
+    return res.send(username + " successfully logged in!")
 });
 
 // Add a book review
